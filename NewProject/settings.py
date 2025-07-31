b01ce969e8b43b432b2edf7d9fcaa04740050de1
ShivaReddy4 <shivareddythurpu16@gmail.com>
2025-07-31 14:47:45 +0530

    Update settings.py

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -49,7 +49,12 @@ import os
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['newproject-api-p1s2.onrender.com', 'localhost', '127.0.0.1']
+import os
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 
