0e5bc1a9e23c9247feaa864109bd4b684214b40b
Shivareddy Singireddy <shivareddythurpu16@gmail.com>
2025-07-31 15:05:04 +0530

    Update ALLOWED_HOSTS for Render

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -49,12 +49,6 @@ import os
 DEBUG = False
 
 
-import os
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 
